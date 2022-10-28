403273f9da0fd3534870f5da84a9aa3666fa49df
bpfree <brian.patrick.free@gmail.com>
2022-10-28 14:49:55 -0400

    Create vessel traffic rasters and normalization

--- code/5_vessel_traffic.R
+++ code/5_vessel_traffic.R
@@ -24,43 +24,107 @@ pacman::p_load(dplyr,
 # Set directories
 ## Define data directory (as this is an R Project, pathnames are simplified)
 ### Input directory
-ais_tracks_dir <- "data/a_raw_data/AISVesselTransitCounts2019.gdb"
+ais_tracks_dir <- "data/a_raw_data/ais_counts_2019"
 
 ### Output directory
-analysis_gpkg <- "data/c_analysis_data/gom_cable_study.gpkg"
-ais_tracks_gpkg <- "data/b_intermediate_data/gom_vessel.gpkg"
-
-# View layer names within geodatabase
-sf::st_layers(dsn = ais_tracks_dir,
-              do_count = TRUE)
+raster_dir <- "data/d_raster_data"
+intermediate_dir <- "data/b_intermediate_data"
 
 #####################################
 #####################################
 
-# Load study area (to clip habitats to only that area)
-study_area <- st_read(dsn = analysis_gpkg, layer = "gom_study_area_marine")
+# Load AIS data
+## Transit counts: https://marinecadastre.gov/downloads/data/ais/ais2019/AISVesselTransitCounts2019.zip)
+### Metadata: https://www.fisheries.noaa.gov/inport/item/61037
+
+#### Cargo vessels
+cargo_ais2019 <- raster::raster(paste(ais_tracks_dir, "AIS19_Cargo1.tif", sep = "/"))
+
+#### Fishing vessels
+fishing_ais2019 <- raster::raster(paste(ais_tracks_dir, "AIS19_Fishing1.tif", sep = "/"))
+
+#### Passenger vessels
+passenger_ais2019 <- raster::raster(paste(ais_tracks_dir, "AIS19_Passenger1.tif", sep = "/"))
+
+#### Pleasure craft and sailing vessels
+pleasure_ais2019 <- raster::raster(paste(ais_tracks_dir, "AIS19_Pleasure1.tif", sep = "/"))
+
+#### Tanker vessels
+tanker_ais2019 <- raster::raster(paste(ais_tracks_dir, "AIS19_Tanker1.tif", sep = "/"))
+
+#### Tug and tow vessels
+tugtow_ais2019 <- raster::raster(paste(ais_tracks_dir, "AIS19_TugTow1.tif", sep = "/"))
+
+## Vessel tracks (other): https://marinecadastre.gov/downloads/data/ais/ais2019/AISVesselTracks2019.zip
+### Metadata: https://www.fisheries.noaa.gov/inport/item/59927
+
+#### Other vessels
+other_ais2019 <- raster::raster(paste(ais_tracks_dir, "AIS19_Other1.tif", sep = "/"))
 
 #####################################
+#####################################
 
-# Load AIS data (source: https://marinecadastre.gov/downloads/data/ais/ais2019/AISVesselTransitCounts2019.zip)
-## Metadata: https://www.fisheries.noaa.gov/inport/item/61037
-### All vessels
+# Create normalization functions
+## Linear function
+linear_function <- function(raster){
+  # define projection (EPSG:5070)
+  crs <- 5070
+  
+  # reproject into coordinate reference system to match BOEM call areas
+  raster_5070 <- raster::projectRaster(from = raster,
+                                       crs = crs,
+                                       res = 100) # resolution should be put in meters as EPSG:5070 is in meters, no longer degrees
+  
+  # calculate minimum value
+  min <- minValue(raster_5070)
+  
+  # recalculate maximum value
+  max <- maxValue(raster_5070)
+  
+  # create linear function
+  normalize <- (raster_5070[] - min) / (max - min)
+  
+  # set values back to the newly projected raster
+  vessel_normalize <- setValues(raster_5070, normalize)
+  
+  # return the raster
+  return(vessel_normalize)
+}
 
+#####################################
+#####################################
 
-### Cargo vessels
+# Normalize vessel traffic
+cargo_normalized <- cargo_ais2019 %>%
+  linear_function()
 
+fishing_normalized <- fishing_ais2019 %>%
+  linear_function()
 
-### Fishing vessels
+passenger_normalized <- passenger_ais2019 %>%
+  linear_function()
 
+pleasure_normalized <- pleasure_ais2019 %>%
+  linear_function()
 
-### Passenger vessels
+tanker_normalized <- tanker_ais2019 %>%
+  linear_function()
 
+tugtow_normalized <- tugtow_ais2019 %>%
+  linear_function()
 
-### Pleasure craft and sailing vessels
+other_normalized <- other_ais2019 %>%
+  linear_function()
 
+#####################################
+#####################################
 
-### Tanker vessels
-
-
-### Tug and tow vessels
-
+# Export data
+## Raster data
+writeRaster(cargo_normalized, filename = file.path(raster_dir, "cargo_ais2019_normalized.grd"), overwrite = T)
+writeRaster(fishing_normalized, filename = file.path(raster_dir, "fishing_ais2019_normalized.grd"), overwrite = T)
+writeRaster(passenger_normalized, filename = file.path(raster_dir, "passenger_ais2019_normalized.grd"), overwrite = T)
+writeRaster(pleasure_normalized, filename = file.path(raster_dir, "pleasure_ais2019_normalized.grd"), overwrite = T)
+writeRaster(tanker_normalized, filename = file.path(raster_dir, "tanker_ais2019_normalized.grd"), overwrite = T)
+writeRaster(tugtow_normalized, filename = file.path(raster_dir, "tugtow_ais2019_normalized.grd"), overwrite = T)
+writeRaster(other_normalized, filename = file.path(raster_dir, "other_ais2019_normalized.grd"), overwrite = T)
