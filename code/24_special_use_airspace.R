c64dd039b21b6cfb6c8be41d895dd291b661d2a0
bpfree <brian.patrick.free@gmail.com>
2022-11-14 14:03:22 -0500

    Fix source link

--- code/24_special_use_airspace.R
+++ code/24_special_use_airspace.R
@@ -37,7 +37,7 @@ study_area <- st_read(dsn = analysis_gpkg, layer = "gom_study_area_marine")
 
 #####################################
 
-# Load special use airspace data (source: https://hub.arcgis.com/datasets/dd0d1b726e504137ab3c41b21835d05b_0)
+# Load special use airspace data (source: https://ais-faa.opendata.arcgis.com/datasets/dd0d1b726e504137ab3c41b21835d05b_0/)
 airspace <- st_read(dsn = airspace_dir, layer = "Special_Use_Airspace") %>%
   # reproject the coordinate reference system to match study area data (EPSG:5070)
   sf::st_transform("EPSG:5070") %>% # EPSG 5070 (https://epsg.io/5070)
