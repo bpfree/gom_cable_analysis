bc15da56e61fb661458262648833be338a1a78b7
bpfree <brian.patrick.free@gmail.com>
2022-11-23 07:47:30 -0500

    Finalize landing site randomization selection

--- code/31_landing_sites.R
+++ code/31_landing_sites.R
@@ -32,6 +32,7 @@ raster_dir <- "data/d_raster_data"
 
 ### Output directories
 least_cost_gpkg <- "data/e_least_cost_path/least_cost_path_analysis.gpkg"
+landing_sites_gpkg <-"data/b_intermediate_data/landing_sites.gpkg"
 
 #####################################
 #####################################
@@ -53,16 +54,82 @@ coast_points
 #####################################
 #####################################
 
-# Obtain 10 cheapest landing locations
-coast10 <- coast_points %>%
-  # arrange costs
+# Obtain 50 cheapest landing locations
+coast50 <- coast_points %>%
+  # arrange costs  by cost
   dplyr::arrange(cost) %>%
-  # select the bottom 10 percent (- signifies bottom)
-  dplyr::top_frac(-0.05, cost)
+  # select the cheapest 50 (- signifies bottom) to get cheapest locations
+  dplyr::top_n(-50, cost)
 
-coast10
+coast50
+
+#####################################
 
 g <- ggplot() + 
   geom_sf(data = study_area, fill = NA, color = "blue", linetype = "dashed") +
-  geom_sf(data = coast10)
+  geom_sf(data = coast50)
 g
+
+#####################################
+#####################################
+
+# Subset 4 landing option locations
+## This will use endID location [list(unique(coast50$endID)) --> 303708, 302357, 304846, 300463]
+landing_area_303708 <- coast50 %>%
+  # filter by endID "303708"
+  dplyr::filter(endID == 303708) %>%
+  # randomly sample one of the points
+  dplyr::sample_n(1)
+
+landing_area_302357 <- coast50 %>%
+  # filter by endID "302357"
+  dplyr::filter(endID == 302357) %>%
+  # randomly sample one of the points
+  dplyr::sample_n(1)
+
+landing_area_304846 <- coast50 %>%
+  # filter by endID "304846"
+  dplyr::filter(endID == 304846) %>%
+  # randomly sample one of the points
+  dplyr::sample_n(1)
+
+landing_area_300463 <- coast50 %>%
+  # filter by endID "300463"
+  dplyr::filter(endID == 300463) %>%
+  # randomly sample one of the points
+  dplyr::sample_n(1)
+
+#####################################
+
+g <- ggplot() + 
+  geom_sf(data = study_area, fill = NA, color = "blue", linetype = "dashed") +
+  geom_sf(data = coast50) +
+  geom_sf(data = landing_area_303708, color = "red") +
+  geom_sf(data = landing_area_302357, color = "orange") +
+  geom_sf(data = landing_area_304846, color = "yellow") +
+  geom_sf(data = landing_area_300463, color = "darkred")
+  
+g
+
+#####################################
+#####################################
+
+# Combine landing sites
+landing_areas <- landing_area_300463 %>%
+  rbind(landing_area_302357,
+        landing_area_303708,
+        landing_area_304846)
+
+#####################################
+#####################################
+
+# Export data
+## Least cost geopackage
+st_write(obj = landing_areas, dsn = least_cost_gpkg, "landing_areas", append = F)
+
+## Landing sites geopackage
+st_write(obj = landing_areas, dsn = landing_sites_gpkg, "landing_areas", append = F)
+st_write(obj = landing_area_300463, dsn = landing_sites_gpkg, "landing_area_300463", append = F)
+st_write(obj = landing_area_302357, dsn = landing_sites_gpkg, "landing_area_302357", append = F)
+st_write(obj = landing_area_303708, dsn = landing_sites_gpkg, "landing_area_303708", append = F)
+st_write(obj = landing_area_304846, dsn = landing_sites_gpkg, "landing_area_304846", append = F)
