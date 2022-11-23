c10bbb14ae4d7c5db86f93498ea71dd1ec23c4a3
bpfree <brian.patrick.free@gmail.com>
2022-11-23 07:57:24 -0500

    Delete end point script

--- code/x_define_ending_points.R
+++
@@ -1,7 +0,0 @@
-#################################
-### X. Define Ending Point(s) ###
-#################################
-
-## Coastline boundary
-coastline <- rnaturalearth::ne_coastline(scale = 10, returnclass = "sf") %>%
-  sf::st_transform("EPSG:5070")
