06133f456e2d450aee44990e473f237769e32a47
bpfree <brian.patrick.free@gmail.com>
2022-11-29 17:16:30 -0500

    Cost layer to include new data that match correct columns and rows for summarizing

--- code/tentative_analysis/cost_analysis.R
+++ code/tentative_analysis/cost_analysis.R
@@ -44,14 +44,14 @@ sf::st_layers(dsn = data_dir,
 #####################################
 
 # Load data
+study_area <- st_read(dsn = data_dir, layer = "gom_study_area_marine")
+
 ## Raster grid
 gom_raster <- raster::raster(paste(raster_dir, "gom_study_area_marine_100m_raster.grd", sep = "/"))
 
 ## Constraints grid
 constraints <- raster::raster(paste(least_cost_dir, "constraints_raster.grd", sep = "/"))
 arcgis_constraints <- raster::raster(paste(least_cost_dir, "arc_constraints_raster.grd", sep = "/"))
-all_constraints <- sf::st_read(dsn = least_cost_gpkg, layer = "all_constraints") %>%
-  as("Spatial")
 
 ## National Security
 ### Special use Airspace
@@ -68,6 +68,13 @@ fish_haven <- st_read(dsn = data_dir, layer = "fish_havens") %>%
   dplyr::mutate(value = 0.3) %>%
   fasterize(raster = gom_raster,
             field = "value")
+
+prd_species <- st_read(dsn = data_dir, layer = "prd_species") %>%
+  fasterize(raster = gom_raster,
+            field = "value")
+
+pelagic_birds <- raster::raster(paste(raster_dir, "pelagic_normalize.grd", sep = "/"))
+extent(pelagic_birds) <- extent(gom_raster)
 
 ### Potentially Sensitive Biological Features and Low Relief Features
 psbf_lrf <- st_read(dsn = data_dir, layer = "psbf_lrf") %>%
@@ -112,6 +119,56 @@ nexrad70km <- st_read(dsn = data_dir, layer = "nexrad70km") %>%
   fasterize(raster = gom_raster,
             field = "value")
 
+### Aids to Navigation
+navigation_aid <- sf::st_read(dsn = data_dir, layer = "aids_to_navigation") %>%
+  # add cost value of 0.75
+  dplyr::mutate(value = 0.75) %>%
+  fasterize(raster = gom_raster,
+            field = "value")
+
+### Shipping Lanes
+shipping_lane <- sf::st_read(dsn = data_dir, layer = "shipping_lane") %>%
+  # add cost value of 0.75
+  dplyr::mutate(value = 0.75) %>%
+  fasterize(raster = gom_raster,
+            field = "value")
+
+### Pipelines
+pipeline <- sf::st_read(dsn = data_dir, layer = "pipelines") %>%
+  # add cost value of 0.75
+  dplyr::mutate(value = 0.75) %>%
+  fasterize(raster = gom_raster,
+            field = "value")
+
+### AIS
+#### Cargo 2019
+ais_cargo <- raster::raster(paste(raster_dir, "cargo_ais2019_normalized.grd", sep = "/"))
+extent(ais_cargo) <- extent(gom_raster)
+
+#### Fishing 2019
+ais_fishing <- raster::raster(paste(raster_dir, "fishing_ais2019_normalized.grd", sep = "/"))
+extent(ais_fishing) <- extent(gom_raster)
+
+#### Passenger 2019
+ais_passenger <- raster::raster(paste(raster_dir, "passenger_ais2019_normalized.grd", sep = "/"))
+extent(ais_passenger) <- extent(gom_raster)
+
+#### Pleasure 2019
+ais_pleasure <- raster::raster(paste(raster_dir, "pleasure_ais2019_normalized.grd", sep = "/"))
+extent(ais_pleasure) <- extent(gom_raster)
+
+#### Tanker 2019
+ais_tanker <- raster::raster(paste(raster_dir, "tanker_ais2019_normalized.grd", sep = "/"))
+extent(ais_tanker) <- extent(gom_raster)
+
+#### Tugtow 2019
+ais_tugtow <- raster::raster(paste(raster_dir, "tugtow_ais2019_normalized.grd", sep = "/"))
+extent(ais_tugtow) <- extent(gom_raster)
+
+#### Other 2019
+ais_other <- raster::raster(paste(raster_dir, "other_ais2019_normalized.grd", sep = "/"))
+extent(ais_other) <- extent(gom_raster)
+
 ## Fisheries
 ### Menhaden
 menhaden <- raster::raster(paste(raster_dir, "menhaden_2000_2019_normalize.grd", sep = "/"))
@@ -137,11 +194,23 @@ extent(slope) <- extent(gom_raster)
 ## cover any NA values of another raster with values from any other raster (all barrier cells)
 cost_raster <- raster::brick(special_use_airspace,
                              fish_haven,
+                             prd_species,
+                             pelagic_birds,
                              psbf_lrf,
                              boem_psbf,
                              coral_hapc,
                              lightering_zones,
                              not_carbon_capture,
+                             navigation_aid,
+                             shipping_lane,
+                             pipeline,
+                             ais_cargo,
+                             ais_fishing,
+                             ais_passenger,
+                             ais_pleasure,
+                             ais_tanker,
+                             ais_tugtow,
+                             #ais_other,
                              nexrad70km,
                              menhaden,
                              bathymetry,
@@ -167,7 +236,7 @@ plot(arc_gis_cost_raster)
 
 ## Inspect new raster
 minValue(cost_raster) # 0
-maxValue(cost_raster) # maximum value = 3.39529
+maxValue(cost_raster) # maximum value = 6.162601
 list(unique(cost_raster)) # list all unique values
 res(cost_raster) # 100 x 100
 hist(cost_raster) # show histogram of values (though mostly values near 1)
