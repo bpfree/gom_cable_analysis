e0874f3481875dc5e4c67b9da071c89b2bdaf294
bpfree <brian.patrick.free@gmail.com>
2022-11-30 16:49:49 -0500

    Least cost path using movecost

--- code/tentative_analysis/lcp_movecost.R
+++ code/tentative_analysis/lcp_movecost.R
@@ -10,6 +10,7 @@ if (!require("pacman")) install.packages("pacman")
 pacman::p_load(dplyr,
                fasterize,
                ggplot2,
+               ggspatial,
                movecost,
                plyr,
                ncdf4, # can be used to read the bathymetry data (as they are an netCDF file [.nc])
@@ -20,6 +21,13 @@ pacman::p_load(dplyr,
                sp,
                terra,
                tidyr)
+
+#####################################
+
+# Movecost Documentation
+## Manual: https://cran.r-project.org/web/packages/movecost/movecost.pdf
+## Github: https://github.com/cran/movecost
+## Paper: https://www.sciencedirect.com/science/article/pii/S2352711019302341
 
 #####################################
 #####################################
@@ -49,11 +57,27 @@ sf::st_layers(dsn = least_cost_gpkg,
 #####################################
 
 # Load data
-starting_points <- sf::st_read(dsn = least_cost_gpkg, layer = "starting_site")
-landing_points <- sf::st_read(dsn = least_cost_gpkg, layer = "landing_areas")
+starting_points <- sf::st_read(dsn = least_cost_gpkg, layer = "starting_site") %>%
+  # convert to SpatialPointsDataFrame for use in movecoast()
+  sf::as_Spatial()
+landing_points <- sf::st_read(dsn = least_cost_gpkg, layer = "landing_areas") %>%
+  # convert to SpatialPointsDataFrame for use in movecoast()
+  sf::as_Spatial()
 
 cost_raster <- raster::raster(paste(least_cost_dir, "cost_raster.grd", sep = "/"))
-barrier_raster <- raster::raster(paste(least_cost_dir, "constraints_raster.grd", sep = "/"))
+arc_cost_raster <- raster::raster(paste(least_cost_dir, "arcgis_cost_raster.grd", sep = "/"))
+barrier <- sf::st_read(dsn = least_cost_gpkg, "all_constraints") %>%
+  # convert to SpatialPolygonsDataFrame for use in movecoast()
+  sf::as_Spatial()
+
+#####################################
+
+# when as sf
+g <- ggplot() + 
+  ggspatial::geom_spatial(data = starting_points, color = "blue") +
+  geom_sf(data = landing_points, color = "red") +
+  geom_sf(data = barrier, fill = NA, linetype = "dashed")
+g
 
 #####################################
 #####################################
@@ -69,12 +93,34 @@ lcp <- movecost::movecost(dtm = cost_raster,
                           origin = starting_points,
                           # destination are the shore landing points
                           destin = landing_points,
-                          funct = "t",
+                          funct = "ree",
                           # barriers for areas not permitted
-                          barrier = barrier_raster,
+                          barrier = barrier,
                           # have output as raster
                           outp = "r",
                           # number of directions possible (knight and queen's case)
                           # Suitability for cable? Maybe queen's case is more appropriate (8)
-                          move = 16,
-                          field = 0)
+                          move = 8,
+                          field = 0,
+                          # do not return to starting point
+                          return.base = FALSE,
+                          # export graphic
+                          export = TRUE)
+
+lcp2 <- movecost::movecost(dtm = arc_cost_raster,
+                          # origin are offshore wind border points
+                          origin = starting_points,
+                          # destination are the shore landing points
+                          destin = landing_points,
+                          funct = "t",
+                          # barriers for areas not permitted
+                          barrier = barrier,
+                          # have output as raster
+                          outp = "r",
+                          # number of directions possible (knight and queen's case)
+                          # Suitability for cable? Maybe queen's case is more appropriate (8)
+                          move = 8,
+                          # do not return to starting point
+                          return.base = FALSE,
+                          # export graphic
+                          export = FALSE)
