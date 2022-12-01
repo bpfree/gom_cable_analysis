dff516a4f4f90fc9cdcbfc01ce74a7c84b97ba47
bpfree <brian.patrick.free@gmail.com>
2022-12-01 08:47:51 -0500

    Create pathways and graphics using leastcostpath version 2.0.4

--- code/tentative_analysis/lcp_leastcostpath.R
+++ code/tentative_analysis/lcp_leastcostpath.R
@@ -47,7 +47,7 @@ raster_dir <- "data/d_raster_data"
 
 ### Output directories
 tentative_analysis <- "code/tentative_analysis"
-final_data_dir <- "data/f_final_data"
+final_data_gpkg <- "data/f_final_data/gom_cable_final.gpkg"
 
 #####################################
 
@@ -63,47 +63,40 @@ sf::st_layers(dsn = least_cost_gpkg,
 
 # Load data
 ## Starting point
-starting_points <- sf::st_read(dsn = least_cost_gpkg, layer = "starting_site") %>%
-  # convert to SpatialPointsDataFrame for use in movecoast()
-  sf::as_Spatial()
+starting_points <- sf::st_read(dsn = least_cost_gpkg, layer = "starting_site")
 
 ## Landing point
+landing_points <- sf::st_read(dsn = least_cost_gpkg, layer = "landing_areas")
+
 ### 300463
 landing_points_300463 <- sf::st_read(dsn = least_cost_gpkg, layer = "landing_areas") %>%
-  dplyr::filter(endID == 300463) %>%
-  # convert to SpatialPointsDataFrame for use in movecoast()
-  sf::as_Spatial()
+  dplyr::filter(endID == 300463)
 
 ### 302357
 landing_points_302357 <- sf::st_read(dsn = least_cost_gpkg, layer = "landing_areas") %>%
-  dplyr::filter(endID == 302357) %>%
-  # convert to SpatialPointsDataFrame for use in movecoast()
-  sf::as_Spatial()
+  dplyr::filter(endID == 302357)
 
 ### 303708
 landing_points_303708 <- sf::st_read(dsn = least_cost_gpkg, layer = "landing_areas") %>%
-  dplyr::filter(endID == 303708) %>%
-  # convert to SpatialPointsDataFrame for use in movecoast()
-  sf::as_Spatial()
+  dplyr::filter(endID == 303708)
 
 ### 304846
 landing_points_304846 <- sf::st_read(dsn = least_cost_gpkg, layer = "landing_areas") %>%
-  dplyr::filter(endID == 304846) %>%
-  # convert to SpatialPointsDataFrame for use in movecoast()
-  sf::as_Spatial()
+  dplyr::filter(endID == 304846)
 
-cost_raster <- raster::raster(paste(least_cost_dir, "cost_raster.grd", sep = "/"))
+cost_raster <- terra::rast(paste(least_cost_dir, "cost_raster.grd", sep = "/"))
+cost_raster2 <- terra::rast(paste(least_cost_dir, "arcgis_cost_raster.grd", sep = "/"))
 barrier_raster <- raster::raster(paste(least_cost_dir, "constraints_raster.grd", sep = "/"))
 
 #####################################
 #####################################
 
 # create slope cost surface
-slope_cs <- leastcostpath::create_slope_cs(dem = cost_raster,
+slope_cs <- leastcostpath::create_slope_cs(x = cost_raster2,
                                            # functions
                                            cost_function = "tobler",
                                            # neighbors = 4, 8, 16, 32, 48
-                                           neighbours = 4)
+                                           neighbours = 8)
 
 # create barrier cost surface
 barrier_cs <- leastcostpath::create_barrier_cs(raster = cost_raster,
@@ -119,55 +112,113 @@ barrier_cs <- leastcostpath::create_barrier_cs(raster = cost_raster,
 # create cost surface
 cs <- slope_cs * barrier_cs
 
+cs <- leastcostpath::create_cs(x = cost_raster2,
+                               neighbours = 8)
+
 #####################################
 #####################################
 
 # create least cost path
+## Option 1
 ## 300463
-lcp300463 <- leastcostpath::create_lcp(cost_surface = cs,
+lcp300463 <- leastcostpath::create_lcp(x = slope_cs,
                                        origin = starting_points,
                                        destination = landing_points_300463,
-                                       cost_distance = TRUE,
-                                       directional = TRUE) %>%
-  as("sf")
+                                       cost_distance = TRUE)
 
 ## 302357
-lcp302357 <- leastcostpath::create_lcp(cost_surface = cs,
+lcp302357 <- leastcostpath::create_lcp(x = slope_cs,
                                        origin = starting_points,
                                        destination = landing_points_302357,
-                                       cost_distance = TRUE,
-                                       directional = TRUE) %>%
-  as("sf")
+                                       cost_distance = TRUE)
 
 ## 303708
-lcp303708 <- leastcostpath::create_lcp(cost_surface = cs,
+lcp303708 <- leastcostpath::create_lcp(x = slope_cs,
                                        origin = starting_points,
                                        destination = landing_points_303708,
-                                       cost_distance = TRUE,
-                                       directional = TRUE) %>%
-  as("sf")
+                                       cost_distance = TRUE)
 
 ## 304846
-lcp304846 <- leastcostpath::create_lcp(cost_surface = cs,
+lcp304846 <- leastcostpath::create_lcp(x = slope_cs,
                                        origin = starting_points,
                                        destination = landing_points_304846,
-                                       cost_distance = TRUE,
-                                       directional = TRUE) %>%
-  as("sf")
+                                       cost_distance = TRUE)
+
+#####################################
+
+## Option 2
+## 300463
+lcp300463b <- leastcostpath::create_lcp(x = cs,
+                                       origin = starting_points,
+                                       destination = landing_points_300463,
+                                       cost_distance = TRUE)
+
+## 302357
+lcp302357b <- leastcostpath::create_lcp(x = cs,
+                                       origin = starting_points,
+                                       destination = landing_points_302357,
+                                       cost_distance = TRUE)
+
+## 303708
+lcp303708b <- leastcostpath::create_lcp(x = cs,
+                                       origin = starting_points,
+                                       destination = landing_points_303708,
+                                       cost_distance = TRUE)
+
+## 304846
+lcp304846b <- leastcostpath::create_lcp(x = cs,
+                                       origin = starting_points,
+                                       destination = landing_points_304846,
+                                       cost_distance = TRUE)
 
 #####################################
 
 study_area <- st_read(dsn = data_dir, layer = "gom_study_area_marine")
-
-plot(lcp300463)
-plot(lcp302357)
-plot(lcp303708)
-plot(lcp304846)
+offshore_area <- st_read(dsn = data_dir, layer = "gom_wind_area_i")
+barriers <- st_read(dsn = least_cost_gpkg , layer = "all_constraints")
 
 g <- ggplot() +
   geom_sf(data = lcp300463, color = "26596A") +
   geom_sf(data = lcp302357, color = "6E256E") +
   geom_sf(data = lcp303708, color = "552C00") +
   geom_sf(data = lcp304846, color = "91A437") +
-  geom_sf(data = study_area, fill = NA, linetype = "dashed")
+  geom_sf(data = barriers, alpha = 0.2, color = "purple", fill = NA, size = 0.1) +
+  geom_sf(data = study_area, fill = NA, linetype = "dashed") +
+  geom_sf(data = offshore_area, fill = NA, color = "red") +
+  geom_sf(data = starting_points)
 g
+
+g2 <- ggplot() +
+  geom_sf(data = lcp300463b, color = "26596A") +
+  geom_sf(data = lcp302357b, color = "6E256E") +
+  geom_sf(data = lcp303708b, color = "552C00") +
+  geom_sf(data = lcp304846b, color = "91A437") +
+  geom_sf(data = barriers, alpha = 0.5, color = "purple", fill = NA, size = 0.2) +
+  geom_sf(data = study_area, fill = NA, linetype = "dashed") +
+  geom_sf(data = offshore_area, fill = NA, color = "red") +
+  geom_sf(data = starting_points)
+g2
+
+#####################################
+#####################################
+
+# Combine lines
+lcp_lines <- lcp300463 %>%
+  rbind(lcp302357,
+        lcp303708,
+        lcp304846)
+
+lcp_lines_b <- lcp300463b %>%
+  rbind(lcp302357b,
+        lcp303708b,
+        lcp304846b)
+
+#####################################
+#####################################
+
+# Export data
+## Cost paths
+st_write(obj = lcp_lines, dsn = final_data_gpkg, "gom_lcp_lines", append = F)
+st_write(obj = lcp_lines_b, dsn = final_data_gpkg, "gom_lcp_lines_b", append = F)
+
+## Graphic
