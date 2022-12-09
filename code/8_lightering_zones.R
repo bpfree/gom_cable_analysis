03f55f613fb05c352309ce727410f5d2acabdf8e
bpfree <brian.patrick.free@gmail.com>
2022-12-09 18:15:14 -0500

    Clean code

--- code/8_lightering_zones.R
+++ code/8_lightering_zones.R
@@ -24,9 +24,14 @@ pacman::p_load(dplyr,
 
 # Set directories
 ## Define data directory (as this is an R Project, pathnames are simplified)
+### Input directory
 lightering_zone_dir <- "data/a_raw_data/lightering_zone.gpkg"
 
+### Output directories
+#### Analysis directory
 analysis_gpkg <- "data/c_analysis_data/gom_cable_study.gpkg"
+
+#### Intermediate directory
 lightering_zone_gpkg <- "data/b_intermediate_data/lightering_zone.gpkg"
 
 #####################################
@@ -42,21 +47,25 @@ study_area <- st_read(dsn = analysis_gpkg, layer = "gom_study_area_marine")
 ## For more detailed information on lightering zones and coordinates for polygons: https://www.govinfo.gov/content/pkg/CFR-2018-title33-vol2/xml/CFR-2018-title33-vol2-part156.xml#seqnum156.300
 lightering_zones <- st_read(dsn = lightering_zone_dir, layer = "LighteringZone") %>%
   # reproject the coordinate reference system to match study area data
-  st_transform("EPSG:5070") %>% # EPSG 4326 (https://epsg.io/5070)
+  st_transform("EPSG:5070") %>% # EPSG 5070 (https://epsg.io/5070)
   # limit reefs to only those within study area
   st_intersection(study_area) %>%
-  # create field "layer" and populate with description "lightering zone"
+  # create field to define as "lightering zone"
   dplyr::mutate(layer = "lightering zones") %>%
-  # group by "layer" field for summarising data
+  # group all features by the "layer" and "value" fields to then have a single feature
+  # "value" will get pulled in from the study area layer
   dplyr::group_by(layer,
                   value) %>%
   # summarise data
   dplyr::summarise()
 
+#####################################
+
 g <- ggplot() +
   geom_sf(data = lightering_zones)
 g
 
+#####################################
 #####################################
 
 # Export data
