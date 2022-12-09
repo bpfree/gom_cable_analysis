f0d0c91663dc8ddeb256a80ca7933ef1a024e874
bpfree <brian.patrick.free@gmail.com>
2022-12-09 18:15:53 -0500

    Temporary / tentatitive data

--- code/tentative_analysis/constraints_layer.R
+++ code/tentative_analysis/constraints_layer.R
@@ -1,6 +1,6 @@
-########################
-### Constraints Data ###
-########################
+############################
+### 36. Constraints Data ###
+############################
 
 # Clear environment
 rm(list = ls())
@@ -29,12 +29,12 @@ data_dir <- "data/c_analysis_data/gom_cable_study.gpkg"
 raster_dir <- "data/d_raster_data"
 
 ### Output directories
-intermediate_dir <- "data/b_intermediate_data"
-tentative_dir <- "code/tentative_analysis"
+#### Least Cost Path directories
 least_cost_dir <- "data/e_least_cost_path"
 least_cost_gpkg <- "data/e_least_cost_path/least_cost_path_analysis.gpkg"
 
-#####################################
+#### Intermediate directory
+intermediate_dir <- "data/b_intermediate_data"
 
 # View layer names within geodatabase
 sf::st_layers(dsn = data_dir,
@@ -44,7 +44,7 @@ sf::st_layers(dsn = data_dir,
 #####################################
 
 # Load raster grid
-gom_raster <- raster::raster(paste(raster_dir, "gom_study_area_marine_100m_raster.grd", sep = "/"))
+gom_raster <- terra::rast(paste(raster_dir, "gom_study_area_marine_100m_raster.grd", sep = "/"))
 
 #####################################
 
@@ -74,6 +74,7 @@ environmental_sensor <- sf::st_read(dsn = data_dir, layer = "environmental_senso
 
 # Create constraints layers
 all_constraints <- seagrass %>%
+  # combine constraints datasets so that each is an unique row
   rbind(oyster,
         conservation_area,
         artificial_reef,
@@ -85,90 +86,97 @@ all_constraints <- seagrass %>%
         oil_gas_lease_area,
         drilling_platform,
         environmental_sensor) %>%
+  # create field called "layer" and fill with "barrier" for summary
   dplyr::mutate(barrier = "barrier") %>%
+  # group all features by the "layer" and "value" fields to then have a single feature
   dplyr::group_by(barrier,
                   value) %>%
+  # summarise data to obtain single feature that will act as a barrier
   dplyr::summarise()
-                                 
 
 # Convert to rasters
 ## Environmental
-seagrass_raster <- fasterize(sf = seagrass,
-                             raster = gom_raster,
-                             field = "value")
-
-oyster_raster <- fasterize(sf = oyster,
-                           raster = gom_raster,
-                           field = "value")
-
-## Geophysical
-conservation_area_raster <- fasterize(sf = conservation_area,
-                                      raster = gom_raster,
-                                      field = "value")
-
-artificial_reef_raster <- fasterize(sf = artificial_reef,
-                                    raster = gom_raster,
+seagrass_raster <- terra::rasterize(x = seagrass,
+                                    y = gom_raster,
                                     field = "value")
 
-significant_sediment_raster <- fasterize(sf = significant_sediment,
-                                         raster = gom_raster,
-                                         field = "value")
+oyster_raster <- terra::rasterize(x = oyster,
+                                  y = gom_raster,
+                                  field = "value")
+
+## Geophysical
+conservation_area_raster <- terra::rasterize(x = conservation_area,
+                                             y = gom_raster,
+                                             field = "value")
+
+artificial_reef_raster <- terra::rasterize(x = artificial_reef,
+                                           y = gom_raster,
+                                           field = "value")
+
+significant_sediment_raster <- terra::rasterize(x = significant_sediment,
+                                                y = gom_raster,
+                                                field = "value")
 
 ## Navigational
-unexploded_ordnance_raster <- fasterize(sf = unexploded_ordnance,
-                                        raster = gom_raster,
-                                        field = "value")
+unexploded_ordnance_raster <- terra::rasterize(x = unexploded_ordnance,
+                                               y = gom_raster,
+                                               field = "value")
 
-no_activity_zone_raster <- fasterize(sf = no_activity_zone,
-                                     raster = gom_raster,
-                                     field = "value")
+no_activity_zone_raster <- terra::rasterize(x = no_activity_zone,
+                                            y = gom_raster,
+                                            field = "value")
 
-anchorage_area_raster <- fasterize(sf = anchorage_area,
-                                   raster = gom_raster,
-                                   field = "value")
+anchorage_area_raster <- terra::rasterize(x = anchorage_area,
+                                          y = gom_raster,
+                                          field = "value")
 
 ## Industry
-borehole_raster <- fasterize(sf = borehole,
-                             raster = gom_raster,
-                             field = "value")
+borehole_raster <- terra::rasterize(x = borehole,
+                                    y = gom_raster,
+                                    field = "value")
 
-oil_gas_lease_area_raster <- fasterize(sf = oil_gas_lease_area,
-                                       raster = gom_raster,
-                                       field = "value")
+oil_gas_lease_area_raster <- terra::rasterize(x = oil_gas_lease_area,
+                                              y = gom_raster,
+                                              field = "value")
 
-drilling_platform_raster <- fasterize(sf = drilling_platform,
-                                      raster = gom_raster,
-                                      field = "value")
+drilling_platform_raster <- terra::rasterize(x = drilling_platform,
+                                             y = gom_raster,
+                                             field = "value")
 
-environmental_sensor_raster <- fasterize(sf = environmental_sensor,
-                                         raster = gom_raster,
-                                         field = "value")
+environmental_sensor_raster <- terra::rasterize(x = environmental_sensor,
+                                                y = gom_raster,
+                                                field = "value")
 
 #####################################
 #####################################
 
 # Create constraints layer
 ## cover any NA values of another raster with values from any other raster (all barrier cells)
-constraints <- raster::cover(seagrass_raster,
-                             oyster_raster,
-                             conservation_area_raster,
-                             artificial_reef_raster,
-                             significant_sediment_raster,
-                             unexploded_ordnance_raster,
-                             no_activity_zone_raster,
-                             anchorage_area_raster,
-                             borehole_raster,
-                             oil_gas_lease_area_raster,
-                             drilling_platform_raster,
-                             environmental_sensor_raster)
-
-#####################################
-
-## Create a constraints layer for ArcGIS
-constraints_arc <- constraints
+constraints <- terra::cover(x = seagrass_raster,
+                            y = oyster_raster) %>%
+  # cover with conservation area raster
+  terra::cover(y = conservation_area_raster) %>%
+  # cover with artificial reef raster
+  terra::cover(y = artificial_reef_raster) %>%
+  # cover with significant sediment raster
+  terra::cover(y = significant_sediment_raster) %>%
+  # cover with unexploded ordnance raster
+  terra::cover(y = unexploded_ordnance_raster) %>%
+  # cover with no activity zone raster
+  terra::cover(y = no_activity_zone_raster) %>%
+  # cover with anchorage area raster
+  terra::cover(y = anchorage_area_raster) %>%
+  # cover with borehole raster
+  terra::cover(y = borehole_raster) %>%
+  # cover with active oil and gas lease area raster
+  terra::cover(y = oil_gas_lease_area_raster) %>%
+  # cover with drilling platform raster
+  terra::cover(y = drilling_platform_raster) %>%
+  # cover with environmental sensore raster
+  terra::cover(y = environmental_sensor_raster)
 
 ## Set values of 0 to be 99 for later cost analysis
-constraints_arc[constraints_arc == 0] <- 99
+constraints[constraints == 0] <- 99
 
 #####################################
 #####################################
@@ -178,7 +186,4 @@ constraints_arc[constraints_arc == 0] <- 99
 st_write(obj = all_constraints, dsn = least_cost_gpkg, "all_constraints", append = F)
 
 ## Raster data
-writeRaster(constraints, filename = file.path(tentative_dir, "constraints_raster.grd"), overwrite = T)
 writeRaster(constraints, filename = file.path(least_cost_dir, "constraints_raster.grd"), overwrite = T)
-
-writeRaster(constraints_arc, filename = file.path(least_cost_dir, "arc_constraints_raster.grd"), overwrite = T)
